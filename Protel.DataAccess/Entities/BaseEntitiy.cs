e43bd26e17da7d6c1591aa0b6759267f93668205
Eren İnal <eren.inal@a101.com.tr>
2022-03-24 20:02:40 +0300

    Add project files.

---
+++ Protel.DataAccess/Entities/BaseEntitiy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protel.DataAccess.Entities
+{
+    public class BaseEntitiy
+    {
+        public Int64 Id { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? CreateDate { get; set; } = DateTime.Now;
+        public DateTime? UpdateDate { get; set; }
+
+    }
+}
