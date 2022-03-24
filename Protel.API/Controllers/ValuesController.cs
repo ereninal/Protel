a9bd4c6a3a6739f66ada5c0b2ab5c83f35ee0a7d
Eren İnal <eren.inal@a101.com.tr>
2022-03-24 20:20:59 +0300

    fixed.

---
+++ Protel.API/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Protel.Core.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Protel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("GetEncrypt")]
+        public string GetEncrypt(string param)
+        {
+            return param.Encrypt();
+        }
+        [HttpPost("GetDecrypt")]
+        public string GetDecrypt(string param)
+        {
+            return param.Decrypt();
+        }
+    }
+}
--- Protel.DataAccess/Context/ProtelContext.cs
+++ Protel.DataAccess/Context/ProtelContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Protel.Core.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace Protel.DataAccess.Context
 {
-    public class ProtelContext
+    public class ProtelContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["connectionString"].Decrypt());
+        }
     }
 }
